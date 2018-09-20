7a0416b22fded619fbe3dacfc2d9a64e0bed20fc
Lucile Meunier <meunier.lucile531@gmail.com>
2018-09-20 16:12:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function yourAnimal(yourAnimal) {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  yourAnimal();
+  yourAnimal = yourAnimal();
   console.log('yourAnimal')}
 }
 
